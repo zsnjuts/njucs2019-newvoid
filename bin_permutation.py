f355c91a95998c36275932c828e9c0c2077f7227
ZhangShuai <1290645535@qq.com>
2019-10-27 20:30:10 +0800

    使用了多线程，并行计算各种取反次数的解

--- bin_permutation.py
+++ bin_permutation.py
@@ -1,3 +1,5 @@
+import queue
+import threading
 from functools import partial
 from itertools import permutations, combinations_with_replacement, chain
 from operator import add, sub, and_, or_, invert
@@ -10,8 +12,11 @@ def get_permut(binstrs: List[str], targetstr: str):
     nums = list(map(partial(int, base=2), binstrs))  # 计算二进制串相应的值
     print(f"现有{nums}，正在寻找能够拼出{target}的方法...")
     cals = [add, sub, and_, or_, invert]  # 所有合法运算符
-    # 遍历所有可能的后缀表达式
-    for ivt_num in range(len(nums)+1):  # 取反放在最外层循环，因为可能不需要这么多次取反就可以组合出来
+    result = queue.Queue(maxsize=1)  # 结果队列
+
+    def solve(ivt_num):
+        """寻找在使用取反操作符ivt_num次情况下的可行解"""
+        print(f"线程{ivt_num}已开始运行...")
         for cal_comb in combinations_with_replacement(cals[0:4], len(nums)-1):
             for permut in permutations(chain(nums, cal_comb, [invert]*ivt_num)):
                 stack = []
@@ -36,8 +41,25 @@ def get_permut(binstrs: List[str], targetstr: str):
                                 tmp = p(stack.pop(), stack.pop())
                                 stack.append(tmp)
                 if flag and len(stack) == 1 and stack[-1] == target:
-                    return permut
-    return False
+                    result.put(permut)  # 找到解就放到结果队列中
+                    print(f"线程{ivt_num}已找到解")
+                    exit()
+        print(f"线程{ivt_num}没有找到解")
+
+    # 多线程遍历所有可能的后缀表达式，每个线程遍历一种取反次数
+    solve_threads = []
+    for ivt_num in range(len(nums)+1):  # 取反放在最外层循环，因为可能不需要这么多次取反就可以组合出来
+        # 设置为daemon线程，这样主线程结束时子线程也会强制结束
+        solve_threads.append(threading.Thread(target=solve, args=(ivt_num,), daemon=True))
+        solve_threads[-1].start()
+
+    # 主线程等待子线程，直到有子线程找到解或者所有子线程全部运行结束
+    while result.empty() and any(tr.is_alive() for tr in solve_threads):
+        pass
+    if result.empty():
+        return False
+    else:
+        return result.get()
 
 
 def permut2str(permut):
