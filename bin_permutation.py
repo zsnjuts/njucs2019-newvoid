f8df86488b4337fba755733a5b4658033bc617f9
ZhangShuai <1290645535@qq.com>
2019-10-29 23:23:06 +0800

    修改了一些代码细节，使得代码更具有通用性，适用于任何一元运算符和二元运算符

--- bin_permutation.py
+++ bin_permutation.py
@@ -1,18 +1,16 @@
 from functools import partial
 from itertools import combinations
 from operator import add, sub, and_, or_, invert
-from typing import List
+from typing import List, Iterable, Callable
 
 
-def get_permut_dfs(nums, cals):
-    """递归地搜索所有合法后缀表达式，返回Iterable，nums为操作数，cals为二元运算符
-    为了避免遍历所有解空间，使用了yield，这样get_permut在循环中找到解之后就可以直接退出
-    若要找到所有解，只需要把所有的yield注释掉，把ans和return相关的注释取消，返回的ans列表就会包括所有可行解"""
+def get_permut_dfs(nums: List[int], uny_ops: Iterable[Callable[[int], int]], bin_ops: Iterable[Callable[[int, int], int]]):
+    """递归地搜索所有合法后缀表达式，返回Iterable，nums为操作数，uny_ops为一元运算符，bin_ops为二元运算符
+    使用了yield，以避免内存开销过高，而且调用者找到解后停止后此函数就可以直接停止，从而避免遍历整个解空间"""
     if len(nums) <= 1:
         yield (nums[0], )
-        yield (nums[0], invert)
-        # return [(nums[0], ), (nums[0], invert)]
-    # ans = []
+        for uop in uny_ops:
+            yield (nums[0], uop)
     for ln in range(1, len(nums)//2+1):  # 避免重复
         for comb in combinations(range(len(nums)), ln):
             mark = [False for _ in range(len(nums))]  # True的索引与False的索引分别属于两棵子树
@@ -20,27 +18,25 @@ def get_permut_dfs(nums, cals):
                 mark[c] = True
             nums1 = [nums[i] for i in range(len(nums)) if mark[i]]  # 子树1
             nums2 = [nums[i] for i in range(len(nums)) if not mark[i]]  # 子树2
-            eqs1 = get_permut_dfs(nums1, cals)
-            eqs2 = get_permut_dfs(nums2, cals)
+            eqs1 = get_permut_dfs(nums1, uny_ops, bin_ops)
+            eqs2 = get_permut_dfs(nums2, uny_ops, bin_ops)
             for eq1 in eqs1:
                 for eq2 in eqs2:
-                    for cal in cals:
-                        yield eq1 + eq2 + (cal,)
-                        yield eq1 + eq2 + (cal, invert)
-                        yield eq2 + eq1 + (cal,)
-                        yield eq2 + eq1 + (cal, invert)
-                        # ans.append(eq1 + eq2 + (cal,))
-                        # ans.append(eq1 + eq2 + (cal, invert))
-                        # ans.append(eq2 + eq1 + (cal,))
-                        # ans.append(eq2 + eq1 + (cal, invert))
-    # return ans
+                    for bop in bin_ops:
+                        yield eq1 + eq2 + (bop, )
+                        yield eq2 + eq1 + (bop, )
+                        for uop in uny_ops:
+                            yield eq1 + eq2 + (bop, uop)
+                            yield eq2 + eq1 + (bop, uop)
 
 
 def get_permut(binstrs: List[str], targetstr: str):
     """找到合法的后缀表达式，binstrs为现在有哪些二进制串，targetstr为想要拼成哪个二进制串"""
     target = int(targetstr, base=2)
     nums = list(map(partial(int, base=2), binstrs))  # 计算二进制串相应的值
-    for equation in get_permut_dfs(nums, [add, sub, and_, or_]):
+    uny_ops = [invert]  # 一元运算符
+    bin_ops = [add, sub, and_, or_]  # 二元运算符
+    for equation in get_permut_dfs(nums, uny_ops, bin_ops):
         stack = []
         for p in equation:
             if isinstance(p, int):
@@ -49,12 +45,12 @@ def get_permut(binstrs: List[str], targetstr: str):
                 if len(stack) < 1:
                     break
                 elif len(stack) < 2:
-                    if p == invert:
+                    if p in uny_ops:
                         stack[-1] = p(stack[-1])
                     else:
                         break
                 else:
-                    if p == invert:
+                    if p in uny_ops:
                         stack[-1] = p(stack[-1])
                     else:
                         tmp = p(stack.pop(), stack.pop())
@@ -67,7 +63,8 @@ def get_permut(binstrs: List[str], targetstr: str):
 
 def permut2str(permut):
     """将后缀表达式转为正常表达式"""
-    calsign = {add: '+', sub: '-', and_: '&', or_: '|'}
+    uny_op_str = {invert: '~'}  # 一元操作符对应的字符串表示，这里默认一元操作符都放在操作数前面
+    bin_op_str = {add: '+', sub: '-', and_: '&', or_: '|'}  # 二元操作符对应的字符串表示
     bin_stack, dec_stack = [], []  # 分别用二进制和十进制表示数字，执行完全一样的操作
     for p in permut:
         if isinstance(p, int):
@@ -77,18 +74,18 @@ def permut2str(permut):
             if len(bin_stack) < 1:
                 return ""
             elif len(bin_stack) < 2:
-                if p == invert:
-                    bin_stack[-1] = f"~({bin_stack[-1]})"
-                    dec_stack[-1] = f"~({dec_stack[-1]})"
+                if p in uny_op_str:
+                    bin_stack[-1] = f"{uny_op_str[p]}({bin_stack[-1]})"
+                    dec_stack[-1] = f"{uny_op_str[p]}({dec_stack[-1]})"
                 else:
                     return ""
             else:
-                if p == invert:
-                    bin_stack[-1] = f"~({bin_stack[-1]})"
-                    dec_stack[-1] = f"~({dec_stack[-1]})"
+                if p in uny_op_str:
+                    bin_stack[-1] = f"{uny_op_str[p]}({bin_stack[-1]})"
+                    dec_stack[-1] = f"{uny_op_str[p]}({dec_stack[-1]})"
                 else:
-                    bin_stack.append("(" + bin_stack.pop() + calsign[p] + bin_stack.pop() + ")")
-                    dec_stack.append("(" + dec_stack.pop() + calsign[p] + dec_stack.pop() + ")")
+                    bin_stack.append("(" + bin_stack.pop() + bin_op_str[p] + bin_stack.pop() + ")")
+                    dec_stack.append("(" + dec_stack.pop() + bin_op_str[p] + dec_stack.pop() + ")")
     if len(bin_stack) == 1:
         return bin_stack[-1], dec_stack[-1]
     else:
